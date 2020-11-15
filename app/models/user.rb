8ebba8805e62b74d11801aba72b08018afe74398
shamiwizard <leibyuk04mi@gmail.com>
2020-11-15 17:20:50 +0200

    add roles for user, new controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: %i[PLAYER DM ADMIN SUPERADMIN]
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,27 @@
 FactoryBot.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+    # username { Faker::Internet.username }
+    password { '12345user' }
+
+    trait :role_player do
+      role { :PLAYER }
+    end
+
+    trait :role_dm do
+      role { :DM }
+    end
+
+    trait :role_dm do
+      role { :ADMIN }
+    end
+
+    trait :role_admin do
+      role { :ADMIN }
+    end
+
+    trait :role_superadmin do
+      role { :SUPERADMIN }
+    end
   end
 end
