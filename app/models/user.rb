15372c6fd460616e6fff3c4f37b9545d1d133176
shamiwizard <leibyuk04mi@gmail.com>
2020-11-17 23:37:10 +0200

    Change devive sign in, up, add methods for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,19 @@
 class User < ApplicationRecord
+  has_many :games
+
   mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, authentication_keys: [:username]
 
   enum role: %i[PLAYER DM ADMIN SUPERADMIN]
 
-  validates :first_name, length: { minimun: 3, maximum: 50 }, presence: true
-  validates :last_name, length: { minimun: 3, maximum: 50 }, presence: true
-  validates :password, length: { minimun: 6, maximum: 50 }, presence: true
-  validates :password, length: { minimun: 6, maximum: 50 }, presence: true
+  validates :first_name, length: { in: 3..50 }, presence: true
+  validates :last_name, length: { in: 3..50 }, presence: true
+  validates :password, length: { in: 6..30 }, presence: true
+  validates :username, length: { in: 3..30 }, presence: true
+  validates :date_of_birth, presence: true
 
   def age
     Date.current.year - self.date_of_birth.year
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  describe '.age' do
+    it 'return user age' do
+      expect(user.age).to eq(Date.current.year - user.date_of_birth.year)
+    end
+  end
+
+  describe '.full_name' do
+    it 'return user full name' do
+      expect(user.full_name).to eq("#{user.first_name} #{user.last_name}")
+    end
+  end
 end
